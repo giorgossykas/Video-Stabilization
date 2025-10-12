93ee7c8501fdfa670fa25316f908dbfae33b4ef5
giorg <giorgossykas@hotmail.com>
2025-10-12 22:14:57 +0300

    Added time delay for correct fps display

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 #include <math.h>
+#include <chrono>
+#include <thread>
 
 // OpenCV
 #include <opencv2/opencv.hpp>
@@ -25,6 +27,8 @@ int main(int argc, char** argv)
     int h = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_HEIGHT));
     double fps = cap.get(cv::CAP_PROP_FPS);
     int numFrames = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_COUNT));
+    int framePeriod = static_cast<int>(1000.0 / fps); // ms
+    std::cout << "Target FPS: " << fps << " (" << framePeriod << " ms per frame)\n";
 
     // Video writer
     /*
@@ -35,12 +39,26 @@ int main(int argc, char** argv)
 
     // Read and Display frames
     for (int i = 0; i < numFrames; i++) {
+
+        auto start = std::chrono::high_resolution_clock::now();
+
         cv::Mat currFrame, currGray;
         cap >> currFrame;
         if (currFrame.empty()) break;
         cv::cvtColor(currFrame, currGray, cv::COLOR_BGR2GRAY);
         cv::imshow("Shaky Car", currFrame);
-        if (cv::waitKey(1) == 27) break; // press 'ESC' to exit
+
+        // Add delay for fps
+        auto end = std::chrono::high_resolution_clock::now();
+        auto processTime =
+            std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        // Compute remaining delay to maintain target fps
+        int delay_ms = std::max(1, framePeriod - static_cast<int>(processTime));
+        std::cout << delay_ms << std::endl;
+        // Wait for the computed delay (also handles imshow() window events)
+        int key = cv::waitKey(1);
+        //if (key == 27) break; // Esc to exit
+
     }
 
 
